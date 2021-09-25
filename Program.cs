5783ece628c33ac601472973665c1beee529dfc6
LuisSid55 <luiscitty4@gmail.com>
2021-09-25 02:01:00 -0300

    Soma 2 numeros

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3;
+            Console.WriteLine("\n------Soma------");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("\nPrimeiro número: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("\nSegundo número:  ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            n3 = n1 + n2;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"\nA resposta é:    {n3}\n");
+            Console.ResetColor();
+        }
+    }
+}
